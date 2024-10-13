3845995f3bd159a6f1558afa7d30c46d6b0fbac3
Pavlo Dzvonyk <reptile1601@yahoo.com>
2024-10-13 14:01:29 +0200

    modified path to index.html in app.py

--- The_Old_House/app.py
+++ The_Old_House/app.py
@@ -3,8 +3,8 @@ from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return render_template("home.html", content=['paul', 'tom', 'bob'])
+def index():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
